b99b4cd3be22ec78bae800532f0afbb59244d25e
Sean Boland <Quanbook@gmail.com>
2017-09-18 18:39:20 -0600

    Shooting and aiming works

--- arcade-team-10/arcade-team-10/app.ts
+++ arcade-team-10/arcade-team-10/app.ts
@@ -7,7 +7,7 @@
 
 	let keyState: Phaser.Keyboard;
 	let player: Player;
-	let pAim: Phaser.Sprite;
+	//let pAim: Phaser.Sprite;
 
 	var scoreText;
 	function preload()
@@ -18,6 +18,7 @@
 		game.load.image('pLeft', 'assets/Testchar_left.png');
 		game.load.image('pDown', 'assets/Testchar_down.png');
 		game.load.image('pAim', 'assets/phaser.png');
+		game.load.image('testBullet', 'assets/temp.png');
 	}
 
 	function create()
@@ -28,9 +29,9 @@
 		player = new Player(game);
 		game.add.existing(player);
 
-		pAim = game.add.sprite(player.x + player.width / 2, player.y, 'pAim');
-		pAim.anchor.setTo(0.5, 0.5);
-		pAim.scale.setTo(0.2);
+		//pAim = game.add.sprite(player.x + player.width / 2, player.y, 'pAim');
+		//pAim.anchor.setTo(0.5, 0.5);
+		//pAim.scale.setTo(0.2);
 
 		//scoreText = game.add.text(5, 3, score);
 	}
@@ -41,7 +42,7 @@
 
 		keyState = game.input.keyboard;
 
-		player.pUpdate(deltaTime, keyState, pAim);
+		player.pUpdate(deltaTime, keyState);
 	}
 
 	function fullScreen()
@@ -59,6 +60,7 @@ class Player extends Phaser.Sprite
 	pVelocityX: number;
 	pVelocityY: number;
 	pSpeed: number;
+	weapon: Phaser.Weapon;
 
 	constructor(game: Phaser.Game)
 	{
@@ -68,15 +70,19 @@ class Player extends Phaser.Sprite
 
 		this.game.physics.enable(this, Phaser.Physics.ARCADE);
 		this.body.collideWorldBounds = true;
-		this.maxHealth = 5;
+		this.maxHealth = 1;
 
 		this.aim = false;
 		this.pVelocityX = 0;
 		this.pVelocityY = 0;
 		this.pSpeed = 500;
+
+		this.weapon = game.add.weapon(100, 'testBullet');
+		this.weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
+		this.weapon.bulletSpeed = 200;
 	}
 
-	pUpdate(time: number, keyState: Phaser.Keyboard, pAim: Phaser.Sprite)
+	pUpdate(time: number, keyState: Phaser.Keyboard)
 	{
 		this.pVelocityX = 0;
 		this.pVelocityY = 0;
@@ -86,7 +92,9 @@ class Player extends Phaser.Sprite
 			this.aim = true;
 		}
 
-		pAim.position.setTo(this.position.x, this.position.y);
+		//pAim.position.setTo(this.position.x, this.position.y);
+		this.weapon.trackSprite(this, 0, 0);
+		this.weapon.fireAngle = 0;
 
 		if (!this.aim)
 		{
@@ -119,15 +127,18 @@ class Player extends Phaser.Sprite
 				if (keyState.isDown(Phaser.KeyCode.S))
 				{
 					this.pVelocityY += this.pSpeed;
+					this.weapon.fireAngle = 90;
 				}
 
 				if (keyState.isDown(Phaser.KeyCode.A))
 				{
 					this.pVelocityX -= this.pSpeed;
+					this.weapon.fireAngle = 180;
 				}
 				if (keyState.isDown(Phaser.KeyCode.D))
 				{
 					this.pVelocityX += this.pSpeed;
+					this.weapon.fireAngle = 0;
 				}
 			}
 		}
@@ -137,42 +148,80 @@ class Player extends Phaser.Sprite
 			{
 				if (keyState.isDown(Phaser.KeyCode.W))
 				{
-					pAim.position.y = pAim.position.y - this.height / 2;
+					//pAim.position.y = pAim.position.y - this.height / 2;
+					this.weapon.trackOffset.y = -this.height / 2;
+					this.weapon.fireAngle = 270;
 				}
 				else
 				{
-					pAim.position.y = pAim.position.y + this.height / 2;
+					//pAim.position.y = pAim.position.y + this.height / 2;
+					this.weapon.trackOffset.y = this.height / 2;
+					this.weapon.fireAngle = 90;
 				}
 
 				if (keyState.isDown(Phaser.KeyCode.A))
 				{
-					pAim.position.x = pAim.position.x - this.width / 2;
+					//pAim.position.x = pAim.position.x - this.width / 2;
+					this.weapon.trackOffset.x = -this.width / 2;
+					if (this.weapon.fireAngle > 180)
+					{
+						this.weapon.fireAngle -= 45;
+					}
+					else
+					{
+						this.weapon.fireAngle += 45;
+					}
 				}
 				else
 				{
-					pAim.position.x = pAim.position.x + this.width / 2;
+					//pAim.position.x = pAim.position.x + this.width / 2;
+					this.weapon.trackOffset.x = this.width / 2;
+					if (this.weapon.fireAngle > 180)
+					{
+						this.weapon.fireAngle += 45;
+					}
+					else
+					{
+						this.weapon.fireAngle -= 45;
+					}
 				}
 			}
 			else
 			{
 				if (keyState.isDown(Phaser.KeyCode.W))
 				{
-					pAim.position.y = pAim.position.y - this.height / 2;
+					//pAim.position.y = pAim.position.y - this.height / 2;
+					this.weapon.trackOffset.y -= this.height / 2;
+					this.weapon.fireAngle = 270;
 				}
 				if (keyState.isDown(Phaser.KeyCode.S))
 				{
-					pAim.position.y = pAim.position.y + this.height / 2;
+					//pAim.position.y = pAim.position.y + this.height / 2;
+					this.weapon.trackOffset.y += this.height / 2;
+					if (this.weapon.fireAngle == 270)
+					{
+						this.weapon.fireAngle = 0;
+					}
+					else
+					{
+						this.weapon.fireAngle = 90;
+					}
 				}
 
 				if (keyState.isDown(Phaser.KeyCode.A))
 				{
-					pAim.position.x = pAim.position.x - this.width / 2;
+					//pAim.position.x = pAim.position.x - this.width / 2;
+					this.weapon.trackOffset.x -= this.width / 2;
+					this.weapon.fireAngle = 180;
 				}
 				if (keyState.isDown(Phaser.KeyCode.D))
 				{
-					pAim.position.x = pAim.position.x + this.width / 2;
+					//pAim.position.x = pAim.position.x + this.width / 2;
+					this.weapon.trackOffset.x += this.width / 2;
+					this.weapon.fireAngle = 0;
 				}
 			}
+			this.weapon.fire();
 		}
 
 		this.body.velocity.y = this.pVelocityY * time;
