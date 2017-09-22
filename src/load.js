a0084bcc96f01e0917eb6a6d84b1b42750245c21
conankzhang <u0409453@utah.edu>
2017-09-21 21:19:53 -0600

    Quick simulation of load

--- src/load.js
+++ src/load.js
@@ -7,7 +7,7 @@ Berzerk.load = function(game, parent)
 	this.background.anchor.setTo(0.5, 0.5);
 	load.add(this.background);
 
-	this.bar = this.game.add.sprite(this.game.world.centerx - 175, this.game.world.centerY - 16, 'loadingBar');
+	this.bar = this.game.add.sprite(this.game.world.centerX - 175, this.game.world.centerY - 16, 'loadingBar');
 	load.add(this.bar);
 };
 
