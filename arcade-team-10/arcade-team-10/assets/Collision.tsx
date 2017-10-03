15665c524f5b94a9d1c71158e676ddf689a25d77
Sean Boland <Quanbook@gmail.com>
2017-10-03 15:06:30 -0600

    New files for map

---
+++ arcade-team-10/arcade-team-10/assets/Collision.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="CollisionTile" tilewidth="64" tileheight="64" tilecount="1" columns="1">
+ <image source="CollisionTile.png" trans="003039" width="68" height="68"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="3" x="0" y="0" width="68" height="68" visible="0"/>
+   <object id="9" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+</tileset>
